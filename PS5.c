55a603356c508f19b8c5c20cfb8e08a43d83cf9b
Prabhatsethi <99499444+Prabhatsethi@users.noreply.github.com>
2022-02-12 14:00:31 +0530

    Add files via upload

---
+++ PS5.c
@@ -0,0 +1,17 @@
+// Q5
+#include<stdio.h>
+int main()
+{
+    float r,d,c,area,PI=3.14;
+    printf("enter the radius of circle: \n");
+    scanf("%f",&r);
+    d=r*2;
+    printf("\n Diameter is %f",d);
+    
+    c=2*PI*r;
+    printf("\n Cicumference is %f",c);
+    
+    a=PI*r*r;
+    printf("\n Area is %f",area);
+return 0;
+}
